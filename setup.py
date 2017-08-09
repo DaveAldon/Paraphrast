a044a1d31c60eb30c8e2e5479e767cfa9ff840ab
DaveAldon <crawford@mail.gvsu.edu>
2017-08-09 11:35:57 -0400

    CI support test.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Paraphrast',
+    version = '0.1',
+    description = 'Translation of CLI Commands',
+    url='https://github.com/DaveAldon/Paraphrast',
+    author = 'David Crawford',
+    packages = find_packages()
+)
